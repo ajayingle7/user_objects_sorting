df4adac76ba39dcd5e5bdc5132a3439ff1bea759
ajayingle <ajayingle109@gmail.com>
2021-11-26 18:54:33 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	tuple.py

--- exception_try_except_block.py
+++ exception_try_except_block.py
@@ -8,10 +8,26 @@ def m1():
     try:
         print("hey brother")
         print(a / b)
-        print("hello guys")
 
-    except ZeroDivisionError as msg:
+        a = open("return.py","r")
+
+
+        print("hello guys")
+        b = [1,2]
+        print(b[1])
+
+
+
+    except (ZeroDivisionError,IOError,SyntaxError,NameError,AttributeError,IndexError) as msg:
         print("except--block", msg)
+
+    finally:
+        print("helloooo")
+        def m2():
+            a = 10
+            b = 20
+            print(a+b)
+        m2()
 
 
 m1()
