2ac32d0694cdd4de0ee141d0f2c5d0a6d9a93ddd
ajayingle <ajayingle109@gmail.com>
2021-12-03 11:56:11 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	tuple.py

--- practice.py
+++ practice.py
@@ -1,1 +1,11 @@
+class A:
 
+    def m1(self):
+        '''
+        hello
+        '''
+
+
+obj1=A()
+
+print(obj1.m1())
---
+++ reverse_string.py
@@ -0,0 +1,9 @@
+a = "Ajay Sanjay Ingle"
+c = a.split()
+print(c)
+
+d = c[::-1]
+print(d)
+
+e = ' '.join(d)
+print(e)
