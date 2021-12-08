c2ec8313eff43d5797af8ceb5d8620d5786339ea
ajayingle <ajayingle109@gmail.com>
2021-12-08 18:52:05 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	tuple.py

---
+++ generator.py
@@ -0,0 +1,98 @@
+def myfun():
+
+    print("this is function starting")
+
+    yield 100
+
+    print("this is miidle of yield")
+
+    yield 200
+
+    print("end of function")
+
+
+a = myfun()
+
+
+for i in a:
+    print(i)
+
+
+
+class A:
+
+    def even(self):
+        print("fun starting")
+        name = "Saurabh"
+        yield 100
+
+        for i in range(10,20):
+            yield i
+
+
+        for i in range(1,51):
+            if i%2==0:
+                yield i
+
+        yield name
+
+        print("end of fun")
+obj1 = A()
+obj2 = obj1.even()
+print(next(obj2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
