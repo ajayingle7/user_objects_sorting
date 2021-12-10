6ca58b7de07e63ece4d77f680db56edb8012f3e7
ajayingle <ajayingle109@gmail.com>
2021-12-10 14:23:03 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	tuple.py

---
+++ file_handling.py
@@ -0,0 +1,17 @@
+f = open("file.py","a")
+
+username = "ajayingle"
+password = "ajay@123"
+
+login_detail = f"username: {username},  password: {password}"
+
+mno = 7770078554
+email = "ajayingle109@gmail.com"
+contact = f"mno {mno}, email {email}"
+
+
+
+f.writelines(contact)
+
+
+
